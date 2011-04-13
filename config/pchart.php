726671429a244dc3565d70296d74e8e9f8a753d1
Jonathan Davis <jonathan.davis@objectivecompany.com>
2011-04-13 17:52:07 -0600

    Added configuration options to the pChart config file.

--- config/pchart.php
+++ config/pchart.php
@@ -1,4 +1,9 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 return array(
-	
+	'data_directory' => '',
+	'cache_settings' => array(
+		'CacheFolder' => 'cache',
+		'CacheIndex' => 'index.db',
+		'CacheDB' => 'cache.db'
+	),
 );
